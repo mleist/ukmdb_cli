42a5a8b79ec31ec3e0031634079a47818cae4594
Markus Leist <markus@lei.st>
2016-07-15 15:25:40 +0200

    Bump version: 0.0.1 → 0.0.2

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ test_requirements = [
 
 setup(
     name='ukmdb_cli',
-    version='0.0.1',
+    version='0.0.2',
     description="UKMDB Command Line Interface.",
     long_description=readme + '\n\n' + history,
     author="Markus Leist",
